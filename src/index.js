ecee4bbcf68d0ce367c041afb80e64a2bdde26e1
Katsiaryna <nooky27@mail.ru>
2021-08-19 14:06:29 -0500

    feat: test done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    let i = 0;
+
+        while(i< expr.length){
+            let a = ''
+            a = expr.slice(i, i+10).replace(/^0+/, '');
+            if(a === '**********'){
+                result = `${result} `
+            }
+            else{
+    
+                let j = 0
+                let str = ''
+                while(j <a.length){
+                    if(a.slice(j, j+2)=== '10'){
+                        str = `${str}.`
+                    }
+                    else{
+                        str = `${str}-`
+                    }
+                    j+=2;
+                    
+                   
+                    }
+                     result = `${result}${MORSE_TABLE[str]}`
+                }
+        i+=10
+        }
+    return result;
+
 }
 
 module.exports = {
